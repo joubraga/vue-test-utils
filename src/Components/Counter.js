cd587ddbb1a0f71d3ffb1a46d1bd796d6fba56e3
Jonathan Braga <jou_braga@hotmail.com>
2018-04-10 11:07:40 -0300

    Click e Find

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,4 +1,12 @@
 export default {
+    template: 
+        ` <div>
+                <h1>
+                    {{ count }}
+                    <button @click="count++" class="increment">Incrementa</button>
+                </h1>
+            </div>
+        `,
     data () {
         return {
             count: 0
--- tests/conter.spec.js
+++ tests/conter.spec.js
@@ -3,8 +3,13 @@ import Counter from '../src/Components/Counter'
 import expect from 'expect'
 
 describe('Testando Contador', () => {
+    let counter = mount(Counter)
     it('valor inicial do contador', () => {
-        let counter = mount(Counter)
         expect(counter.vm.count).toBe(0)
+    })
+
+    it('Valor após clicar no botão de incremento de contagem', () => {
+        counter.find('button.increment').trigger('click')
+        expect(counter.vm.count).toBe(1)
     })
 })
