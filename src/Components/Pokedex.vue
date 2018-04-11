7c175402995238e3d93b0127984c038d53d1da75
Jonathan Braga <jou_braga@hotmail.com>
2018-04-11 14:19:45 -0300

    Fim curso Vue Test Utils

--- src/Components/Pokedex.vue
+++ src/Components/Pokedex.vue
@@ -1,15 +1,34 @@
 <template>
-    <div>
-        <div class="list">
+    <div id="wrapper">
+        <div class="list" v-if="pokes.length === 0">
             Nenhum item a exibir
         </div>
+
+        <table class="poke-list" v-if="pokes.length > 0">
+            <tbody>
+                <tr v-for="poke in pokes">
+                    <td> {{ poke.name }} </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <a href="" @click.prevent="getPokes()"> Listar </a>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
+        methods: {
+            getPokes () {
+                axios.get('/poke/all').then((response) => {
+                    this.pokes = response.data.pokes
+                })
+            }
+        },
         data () {
             return {
+                pokes: []
             }
         }
     }
